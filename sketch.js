182410f0b14f3d41ceede3482a76f4ccd1e3d77a
Dipper98 <134005854+Dip98@users.noreply.github.com>
2024-10-05 20:23:23 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,15 @@
+var viewportWidth  = document.documentElement.clientWidth;
+var viewportHeight = document.documentElement.clientHeight;
+
 function preload(){
   img1 = loadImage("Dipper98_Logo.png")
 }
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(viewportWidth, viewportHeight);
 }
 
 function draw() {
   background(220);
-  image(img1, 0, 0);
+  image(img1, 0, 0, 400, 400);
 }
